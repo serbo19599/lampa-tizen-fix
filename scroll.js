1f3d10bb2aab14f63162a1692ae1be482abe3c9f
serbo19599 <125740468+serbo19599@users.noreply.github.com>
2026-05-01 21:23:15 +0500

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,24 +1,28 @@
 (function () {
     'use strict';
-    if (window.my_recipe_plugin_loaded) return;
-    window.my_recipe_plugin_loaded = true;
 
-    // Создаем экран плагина
-    Lampa.Component.add('my_recipe_list', function (object, exam) {
-        var scroll = new Lampa.Scroll({mask:true, over:true});
-        var html = $('<div></div>');
+    Lampa.Component.add('my_recipes', function (object, exam) {
+        var scroll = new Lampa.Scroll({mask: true, over: true});
+        var html = $('<div class="category-full"></div>');
+
         this.create = function () {
-            var card = Lampa.Template.get('card', {title: 'Рецепт готов!', release_date: 'Тест'});
+            var card = Lampa.Template.get('card', {
+                title: 'Мои Рецепты',
+                release_date: 'Раздел готов к наполнению'
+            });
             card.addClass('card--full');
-            card.on('hover:enter', function () { Lampa.Noty.show('Работает!'); });
             html.append(scroll.render());
             scroll.append(card);
             return html;
         };
+
         this.render = function () { return this.create(); };
-        this.active = function () { 
+        this.active = function () {
             Lampa.Controller.add('content', {
-                toggle: function () { Lampa.Controller.collectionSet(scroll.render()); Lampa.Controller.navigate(); },
+                toggle: function () {
+                    Lampa.Controller.collectionSet(scroll.render());
+                    Lampa.Controller.navigate();
+                },
                 back: function () { Lampa.Activity.backward(); }
             });
             Lampa.Controller.toggle('content');
@@ -26,16 +30,21 @@
         this.back = function () { Lampa.Activity.backward(); };
     });
 
-    // Добавляем кнопку в меню
+    function addMenu() {
+        var menu_item = $('<li class="menu__item selector"><div class="menu__text">РЕЦЕПТЫ</div></li>');
+        menu_item.on('hover:enter', function () {
+            Lampa.Activity.push({
+                title: 'Рецепты',
+                component: 'my_recipes'
+            });
+        });
+        $('.menu .menu__list').append(menu_item);
+    }
+
     var timer = setInterval(function() {
         if (typeof $ !== 'undefined' && $('.menu .menu__list').length) {
             clearInterval(timer);
-            var item = $('<li class="menu__item selector"><div class="menu__text">РЕЦЕПТЫ</div></li>');
-            item.on('hover:enter', function () {
-                Lampa.Activity.push({title: 'Рецепты', component: 'my_recipe_list'});
-            });
-            $('.menu .menu__list').append(item);
+            addMenu();
         }
     }, 1000);
 })();
-
