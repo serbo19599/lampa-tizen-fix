b55072db5d50291b2cf7773576d1f19d8c38b27c
serbo19599 <125740468+serbo19599@users.noreply.github.com>
2026-05-01 21:53:46 +0500

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -8,22 +8,31 @@
         var target = $('.menu__item[data-action="tv"]');
         
         if (target.length > 0 && !target.data('modded')) {
-            target.find('.menu__text').text('МОИ РЕЦЕПТЫ');
+            target.find('.menu__text').text('РЕЦЕПТЫ');
             target.css('color', '#ffeb3b');
 
             target.on('hover:enter click', function (e) {
                 e.preventDefault();
                 e.stopImmediatePropagation();
                 
-                // Ссылка на ваш сайт
-                var url = 'https://serbo19599.github.io/lampa-tizen-fix/'; 
-                
-                // ВМЕСТО перехода в браузер, открываем как Activity внутри Лампы
+                // Вместо window.location.href используем внутренний вызов Лампы
+                Lampa.Component.add('my_site', function (object, exam) {
+                    var html = $('<div class="full-screen"><iframe src="https://serbo19599.github.io/lampa-tizen-fix/" style="width:100%;height:100%;border:none;"></iframe></div>');
+                    this.create = function () { return html; };
+                    this.render = function () { return html; };
+                    this.active = function () {
+                        Lampa.Controller.add('content', {
+                            toggle: function () { Lampa.Controller.collectionSet(html); Lampa.Controller.navigate(); },
+                            back: function () { Lampa.Activity.backward(); }
+                        });
+                        Lampa.Controller.toggle('content');
+                    };
+                    this.back = function () { Lampa.Activity.backward(); };
+                });
+
                 Lampa.Activity.push({
-                    url: url,
                     title: 'Рецепты',
-                    component: 'web_view', // Это заставляет Лампу открыть сайт внутри себя
-                    page: 1
+                    component: 'my_site'
                 });
                 
                 return false;
